c40e4d9bb62642dfaa70e31979d715cf2259adc8
HerberWest4 <herbertcitoxd10@gmail.com>
2023-12-04 11:43:38 -0400

    subido del modelado

---
+++ Models/Hangar.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoBackendHerbert.Models
+{
+    public class Hangar
+    {
+        [Key]
+        public int idHangar { get; set; }
+        public int capacidad { get; set; }
+        public string localizacion { get; set; }
+        
+    }
+}
