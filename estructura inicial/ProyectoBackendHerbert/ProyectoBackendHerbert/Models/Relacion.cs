9c0745386ec168137de27c9de1969aeadfdf19db
HerberWest4 <herbertcitoxd10@gmail.com>
2023-12-04 11:32:40 -0400

    Subiendo estructura Inicial

---
+++ estructura inicial/ProyectoBackendHerbert/ProyectoBackendHerbert/Models/Relacion.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoBackendHerbert.Models
+{
+    public class Relacion
+    {
+        [Key]
+        public string nombre { get; set; }
+        public int idAvion { get; set; }
+        public int numeroLicencia { get; set; }
+        public int idHangar { get; set; }
+        
+
+    }
+}
